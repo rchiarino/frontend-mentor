1c9deabec55e25d4d7f8c8329be3872459797eb4
rchiarino <tynad7@gmail.com>
2023-02-07 16:09:28 -0300

    qr-component

---
+++ README.md
@@ -0,0 +1,9 @@
+# Personal collection of UI challenges
+
+## Overview
+
+This repository is a collection of UI challenges from [Frontend Mentor](https://www.frontendmentor.io/). I'm using this as a way to practice my HTML, CSS, and JavaScript skills.
+
+## Work I have done
+
+- [QR Code component](/qr-code-component/)
---
+++ qr-code-component/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <img src="/image-qr-code.png" alt="QR Code" className="qr" />
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>
+          Scan the QR code to visit Frontend Master and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
